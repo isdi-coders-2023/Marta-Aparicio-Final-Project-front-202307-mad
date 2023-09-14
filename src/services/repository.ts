e5e0c07845e23bacd4d02e5732e9b097856c6704
martiap <martiap93@gmail.com>
2023-09-14 13:02:20 +0200

    Add users repo, logued types and test

---
+++ src/services/repository.ts
@@ -0,0 +1,11 @@
+import { LoginData } from '../model/user';
+import { Logued } from '../types/logued';
+
+export interface Repository<X extends { id: string | number }> {
+  getAll(token: string): Promise<X[]>;
+  getById(id: X['id'], token: string): Promise<X>;
+  create(newData: Omit<X, 'id'>): Promise<X>;
+  update(newData: Partial<X>, token: string): Promise<X>;
+  delete(token: string): Promise<void>;
+  login(data: LoginData): Promise<Logued>;
+}
